56a55f26be123ae4f001fabc6f8093519ee6ca8b
Shene Suelo <shene.rosesuelo@gmail.com>
2026-02-14 10:08:50 +0000

    Implemented ThreadPoolExecutor for task parallelism

--- payroll_parallelism/task_parallelism.py
+++ payroll_parallelism/task_parallelism.py
@@ -11,3 +11,28 @@ def compute_pagibig(salary):
 
 def compute_tax(salary):
     return salary * 0.10
+
+
+from concurrent.futures import ThreadPoolExecutor
+
+def run_task_parallelism(salary):
+    with ThreadPoolExecutor() as executor:
+        future_sss = executor.submit(compute_sss, salary)
+        future_phil = executor.submit(compute_philhealth, salary)
+        future_pagibig = executor.submit(compute_pagibig, salary)
+        future_tax = executor.submit(compute_tax, salary)
+
+        sss = future_sss.result()
+        phil = future_phil.result()
+        pagibig = future_pagibig.result()
+        tax = future_tax.result()
+
+    total = sss + phil + pagibig + tax
+
+    print("=== Task Parallelism Result ===")
+    print("SSS:", sss)
+    print("PhilHealth:", phil)
+    print("Pag-IBIG:", pagibig)
+    print("Tax:", tax)
+    print("Total Deduction:", total)
+
