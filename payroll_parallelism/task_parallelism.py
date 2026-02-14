3854833676029dc8f26efe58b1fe3c59e32e05cd
Erica Monacillo <ericamonacillo09@gmail.com>
2026-02-14 09:35:31 +0000

    Added deduction functions for task parallelism

---
+++ payroll_parallelism/task_parallelism.py
@@ -0,0 +1,13 @@
+# task_parallelism.py
+
+def compute_sss(salary):
+    return salary * 0.045
+
+def compute_philhealth(salary):
+    return salary * 0.025
+
+def compute_pagibig(salary):
+    return salary * 0.02
+
+def compute_tax(salary):
+    return salary * 0.10
